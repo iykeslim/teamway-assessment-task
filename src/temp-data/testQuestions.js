49aca3562914d44ccca9dec004f2bf2c599fe789
iykeslim <iykeslim01@gmail.com>
2022-11-05 13:41:54 +0100

    adding demo data from temporary server

---
+++ src/temp-data/testQuestions.js
@@ -0,0 +1,91 @@
+// Test data: I -> Introversion, E -> Extroversion
+
+var testQuestions = [
+    {
+        question: 'I get uneasy when my space is not clean.',
+        answers: [
+            {
+                type: 'I',
+                item: 'YES, accurately me!'
+            },
+            {
+                type: ' ',
+                item: 'Not sure!'
+            },
+            {
+                type: 'E',
+                item: "NO, not so for me!"
+            }
+        ]
+    },
+    {
+        question: 'when I am invited for a party I get excited before it is time.',
+        answers: [
+            {
+                type: 'E',
+                item: 'YES, accurately me!'
+            },
+            {
+                type: ' ',
+                item: 'Not sure!'
+            },
+            {
+                type: 'I',
+                item: "NO, not so for me!"
+            }
+        ]
+    },
+    {
+        question: 'I am very thorough with my time, if wasted, I get angry.',
+        answers: [
+            {
+                type: 'I',
+                item: 'YES, accurately me!'
+            },
+            {
+                type: ' ',
+                item: 'Not sure!'
+            },
+            {
+                type: 'E',
+                item: "NO, not so for me!"
+            }
+        ]
+    },
+    {
+        question: "Having people around me levels up my energy.",
+        answers: [
+            {
+                type: 'E',
+                item: 'YES, accurately me!'
+            },
+            {
+                type: ' ',
+                item: 'Not sure!'
+            },
+            {
+                type: 'I',
+                item: "NO, not so for me!"
+            }
+        ]
+    },
+    {
+        question: 'I am shy at first but later on I am very expressive.',
+        answers: [
+            {
+                type: 'I',
+                item: 'YES, accurately me!'
+            },
+            {
+                type: ' ',
+                item: 'Not sure!'
+            },
+            {
+                type: 'E',
+                item: "NO, not so for me!"
+            }
+        ]
+    }
+]
+
+export default testQuestions
