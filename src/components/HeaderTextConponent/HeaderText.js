e3baf9c2432502f34afafa16cf5086987c599141
iykeslim <iykeslim01@gmail.com>
2022-11-05 12:43:35 +0100

    creating demo serve and API data

--- src/components/HeaderTextConponent/HeaderText.js
+++ src/components/HeaderTextConponent/HeaderText.js
@@ -4,7 +4,7 @@ const HeaderText = () => {
   return (
     <div className='row'>
           <div className='col d-flex text-center text-success-50 p-2'>
-            <h1 className='text-center shadow'>Teamway Assessment</h1>
+            <h1 className='text-center shadow'>Teamway Assessment Task</h1>
         </div>
     </div>
   )
